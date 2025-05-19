96cd9a512f379ad793cd6d0e193518e49143a574
SYANiDE- <chase.hatch.business@gmail.com>
2025-05-18 19:37:03 -0700

    Added 18200 | Kerberos 5, etype 23, AS-REP; refactor

--- hashcatformat.sh
+++ hashcatformat.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-for line in $(cat -); do 
-    while getopts "m:h" opt; do
-        case $opt in
-            m)
-                case $OPTARG in
-                    13100)
-                        username=$((cut -d '*' -f 2 | awk -F '$' '{print $2 "\\" $1}') <<<$line)
-                        password=$(cut -d ':' -f 2- <<<$line)
-                        echo -e "${username}:${password}"
-                        ;;
-                    *)
-                        echo -e "[!] $OPTARG not yet supported"
-                        exit 0
-                        ;;
-                esac
+IFS=$'\n' readarray -t lines
+
+function looper {
+    OPTARG=$1
+    for line in ${lines[@]}; do 
+        case $OPTARG in
+            13100)
+                # 13100 | Kerberos 5, etype 23, TGS-REP
+                username=$((\
+                    cut -d '*' -f 2 |\
+                    awk -F '$' '{print $2 "\\" $1}') <<<$line)
+                password=$(cut -d ':' -f 2- <<<$line)
+                echo -e "${username}:${password}"
                 ;;
-            h)
-                echo "USAGE: $0 -m [modenum]"
-                exit 0
+            18200)
+                # 18200 | Kerberos 5, etype 23, AS-REP
+                username=$((\
+                    cut -d '$' -f 4 |cut -d ':' -f 1 | tr '@' ' ' |\
+                    awk -F ' ' '{print $2 "\\\\" $1 }') <<<$line)
+                password=$(cut -d ':' -f 3 <<<$line)
+                echo -e "${username}:${password}"
                 ;;
             *)
-                echo "Invalid argument. Supported: (m, h)"
+                echo -e "[!] $OPTARG not yet supported"
                 exit 0
-            ;;
+                ;;
         esac
     done
+}
+
+while getopts "m:h" opt; do
+    case $opt in
+        m) looper $OPTARG ;;
+        h) echo "USAGE: $0 -m [modenum]" ; exit 0 ;;
+        *) echo "Invalid argument. Supported: (m, h)"; exit 0 ;;
+    esac
 done
